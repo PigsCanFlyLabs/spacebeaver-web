638e5be95c27c98476f2d5808735cae87213a9cb
iliast <illia.stepenko@softformance.com>
2022-08-26 14:03:15 +0300

    (feat) fix feedback

--- src/apps/accounts/views/settings.py
+++ src/apps/accounts/views/settings.py
@@ -1,8 +1,11 @@
 from django import forms
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import View
+
+from templated_email import send_templated_mail
 
 from apps.core.consts import ProfileStepsEnum
 
@@ -29,6 +32,20 @@ class SettingsForm(forms.ModelForm):
         fields = ["email", "password"]
 
 
+class ChangeEmailForm(forms.Form):
+    email = forms.EmailField(
+        label="Email",
+        widget=forms.EmailInput(attrs={"placeholder": "Enter your email"}),
+    )
+
+
+class ChangePasswordForm(forms.Form):
+    password_email = forms.EmailField(
+        label="Email",
+        widget=forms.EmailInput(attrs={"placeholder": "Enter your email"}),
+    )
+
+
 class SettingsView(View):
     template_name = "accounts_form.html"
     form_class = SettingsForm
@@ -40,9 +57,46 @@ class SettingsView(View):
                 "password": "****************",
             }
         )
+
+        change_email_form = ChangeEmailForm()
+        change_password_form = ChangePasswordForm()
         return render(
-            request, self.template_name, {**self.base_context, "form": form}
+            request,
+            self.template_name,
+            {
+                **self.base_context,
+                "form": form,
+                "change_email_form": change_email_form,
+                "change_password_form": change_password_form,
+            },
         )
+
+    def post(self, request):
+        print(request.POST)
+
+        form = None
+        change_email = True
+        if "email" in request.POST:
+            form = ChangeEmailForm(request.POST)
+        elif "password_email" in request.POST:
+            form = ChangePasswordForm(request.POST)
+            change_email = False
+
+        if form:
+            if form.is_valid():
+                email_template = (
+                    "change_email_confirmation"
+                    if change_email
+                    else "change_password_confirmation"
+                )
+                send_templated_mail(
+                    email_template,
+                    settings.DEFAULT_FROM_EMAIL,
+                    [request.user.email],
+                    {"username": request.user.full_name, "reset_url": ""},
+                )
+
+        return self.get(request)
 
     @property
     def base_context(self):
--- src/apps/core/templatetags/form_with_helpers.py
+++ src/apps/core/templatetags/form_with_helpers.py
@@ -5,5 +5,17 @@ register = template.Library()
 
 
 @register.inclusion_tag("templatetags/form_with_helpers.html")
-def form_with_helpers(form, action, action_button_name="Create account"):
-    return {"form": form, "action": action, "button_name": action_button_name}
+def form_with_helpers(
+    form,
+    action,
+    action_button_name="Create account",
+    change_email_form=None,
+    change_password_form=None,
+):
+    return {
+        "form": form,
+        "action": action,
+        "button_name": action_button_name,
+        "change_email_form": change_email_form,
+        "change_password_form": change_password_form,
+    }
