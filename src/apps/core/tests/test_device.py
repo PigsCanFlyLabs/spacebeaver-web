8808c71f2c4898d72bbdcb83f1bdb97d47761884
iliast <illia.stepenko@softformance.com>
2022-08-29 14:42:19 +0300

    (feat) tests

--- src/apps/core/tests/test_device.py
+++ src/apps/core/tests/test_device.py
@@ -18,6 +18,12 @@ device_data = {
     "nickname": "Test device",
 }
 
+device2_data = {
+    "serial_number": "1234567",
+    "used": False,
+    "nickname": "Test device",
+}
+
 
 class TestDeviceKeys(TestCase):
     def setUp(self) -> None:
@@ -39,7 +45,7 @@ class TestDeviceKeys(TestCase):
         device.set_nickname("Test change nickname")
         self.assertEqual(device.nickname, "Test change nickname")
 
-    def test_device_assign(self):
+    def test_device_not_exists(self):
         cant_register_device, message = Device.objects.can_register_device(
             device_data["serial_number"]
         )
@@ -53,6 +59,8 @@ class TestDeviceKeys(TestCase):
         )
         self.assertTrue(can_register_device)
 
+    def test_device_assign(self):
+        device = Device.objects.create(**device_data)
         device.assign_to_user(self.user)
         device.save()
 
@@ -61,7 +69,7 @@ class TestDeviceKeys(TestCase):
         )
 
         self.assertTrue(device.is_used)
-        self.assertFalse(cant_register_device)
+        self.assertFalse(can_register_device)
         self.assertEqual(message, "Device with this serial number is used")
         self.assertEqual(device.user.id, self.user.id)
 
@@ -71,5 +79,41 @@ class TestDeviceKeys(TestCase):
         self.assertIsNone(device.nickname)
         self.assertIsNone(device.user)
 
-    def test_device_form(self):
-        pass
+    def test_device_replace(self):
+        device1 = Device.objects.create(**device_data)
+        device2 = Device.objects.create(**device2_data)
+
+        device1.assign_to_user(self.user)
+        device1.set_nickname("device 1")
+        device1.save()
+
+        self.assertTrue(self.user.have_device)
+        self.assertTrue(device1.is_used)
+        self.assertEqual(self.user.id, device1.user.id)
+
+        Device.objects.delete_user_device(self.user)
+
+        device1 = Device.objects.get(
+            serial_number=device_data["serial_number"]
+        )
+
+        self.assertFalse(device1.is_used)
+
+        device2.assign_to_user(self.user)
+        device2.set_nickname("device 2")
+        device2.save()
+
+        self.assertTrue(self.user.have_device)
+        self.assertFalse(device1.is_used)
+        self.assertTrue(device2.is_used)
+        self.assertEqual(self.user.id, device2.user.id)
+
+    def test_change_device_info(self):
+        device = Device.objects.create(**device_data)
+        self.assertEqual(device.serial_number, device_data["serial_number"])
+        self.assertEqual(device.nickname, device_data["nickname"])
+        device.set_nickname("test change nickname")
+        device.serial_number = device2_data["serial_number"]
+        device.save()
+        self.assertEqual(device.nickname, "test change nickname")
+        self.assertEqual(device.serial_number, device2_data["serial_number"])
