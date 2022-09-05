945c3538cf33d387e528ab2e5f22c0c0d11d8ee1
iliast <illia.stepenko@softformance.com>
2022-09-05 20:56:03 +0300

    (feat) tests

--- src/apps/core/tests/test_billing.py
+++ src/apps/core/tests/test_billing.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from unittest.mock import patch
 
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+from django.test import TestCase
 
 from djstripe.models import Account, Customer
 
@@ -13,6 +13,8 @@ from apps.utils.stripe import (
     add_customer_payment_method,
     create_customer,
     create_subscription,
+    delete_subscription,
+    detach_payment_method,
     set_default_payment_method_for_customer,
 )
 
@@ -40,10 +42,18 @@ FAKE_PAYMENT_METHOD_I = load_fixture(
 )
 
 
+def update_payment_method(data):
+    res = deepcopy(data)
+    res["customer"] = "cus_6lsBvm5rJ0zyHc"
+    return res
+
+
 def update_default_payment_method(data):
-    data["invoice_settings"]["default_payment_method"] = FAKE_PAYMENT_METHOD_I[
+    res = deepcopy(data)
+    res["invoice_settings"]["default_payment_method"] = FAKE_PAYMENT_METHOD_I[
         "id"
     ]
+    return res
 
 
 CUSTOMER_DATA = load_fixture("customer_cus_6lsBvm5rJ0zyHc.json")
@@ -52,7 +62,7 @@ SUBSCRIPTION_DATA = load_fixture(
 )
 
 
-class SubscriptionTestCase(TestCase):
+class BaseBillingTestCase(TestCase):
     def setUp(self) -> None:
         self.user = user_model.objects.create(
             full_name=USERDATA["full_name"],
@@ -63,7 +73,10 @@ class SubscriptionTestCase(TestCase):
         self.user.save()
 
         Customer.sync_from_stripe_data(CUSTOMER_DATA)
+        self.customer = Customer.objects.filter(id=CUSTOMER_DATA["id"]).first()
 
+
+class SubscriptionTestCase(BaseBillingTestCase):
     def test_customer_creation(self):
         self.assertEqual(Account.objects.count(), 1)
         self.assertEqual(Customer.objects.count(), 1)
@@ -71,12 +84,12 @@ class SubscriptionTestCase(TestCase):
 
     @patch(
         "stripe.PaymentMethod._cls_attach",
-        return_value=deepcopy(FAKE_PAYMENT_METHOD_I),
+        return_value=update_payment_method(CUSTOMER_DATA),
         autospec=True,
     )
     @patch(
         "stripe.Customer.modify",
-        return_value=deepcopy(update_default_payment_method(CUSTOMER_DATA)),
+        return_value=update_default_payment_method(CUSTOMER_DATA),
         autospec=True,
     )
     @patch(
@@ -84,23 +97,84 @@ class SubscriptionTestCase(TestCase):
         return_value=deepcopy(SUBSCRIPTION_DATA),
         autospec=True,
     )
+    @patch(
+        "stripe.Subscription.delete",
+        return_value=None,
+        autospec=True,
+    )
     def test_create_subscription(
-        self, attach_mock, modify_mock, subscription_mock
+        self, attach_mock, modify_mock, subscription_mock, delete_mock
     ):
-        add_customer_payment_method(
+        # CREATE SUBSCRIPTION FLOW
+        # See core/view/subscriptions.py  CreateSubscriptionAPIView.post
+
+        payment_method = add_customer_payment_method(
             FAKE_PAYMENT_METHOD_I["id"], self.user.customer_id
         )
-        set_default_payment_method_for_customer(
+
+        self.assertEqual(payment_method["customer"], self.user.customer_id)
+
+        customer = set_default_payment_method_for_customer(
             FAKE_PAYMENT_METHOD_I["id"], self.user.customer_id
         )
-        customer = Customer.objects.filter(id=self.user.customer_id).first()
+
         self.assertEqual(
-            customer.invoice_settings["default_payment_method"],
+            customer["invoice_settings"]["default_payment_method"],
             FAKE_PAYMENT_METHOD_I["id"],
         )
-        subscription = create_subscription(STRIPE_PRICE_ID, customer.id)
+        subscription = create_subscription(STRIPE_PRICE_ID, customer["id"])
+
         self.assertEqual(subscription["id"], SUBSCRIPTION_DATA["id"])
         self.assertEqual(
             subscription["items"]["data"][0]["price"],
             SUBSCRIPTION_DATA["items"]["data"][0]["price"],
         )
+        # CANCEL SUBSCRIPTION FLOW
+        # See core/view/billing.py  CancelSubscriptionAPIView.post
+
+        self.assertEqual(delete_subscription(subscription["id"]), None)
+
+
+class PaymentMethod(BaseBillingTestCase):
+    @patch(
+        "stripe.PaymentMethod._cls_attach",
+        return_value=update_payment_method(CUSTOMER_DATA),
+        autospec=True,
+    )
+    @patch(
+        "stripe.Customer.modify",
+        return_value=update_default_payment_method(CUSTOMER_DATA),
+        autospec=True,
+    )
+    @patch(
+        "stripe.Subscription.create",
+        return_value=deepcopy(SUBSCRIPTION_DATA),
+        autospec=True,
+    )
+    @patch(
+        "stripe.PaymentMethod.detach",
+        return_value=None,
+        autospec=True,
+    )
+    def test_update_payment_method(
+        self, attach_mock, modify_mock, subscription_mock, detach_mock
+    ):
+        # UPDATE/DELETE PAYMENT METHOD FLOW
+        # See core/view/billing.py  UpdatePaymentMethodAPIView.post
+        # See core/view/billing.py  DeletePaymentMethodAPIView.post
+
+        self.assertEqual(self.customer.default_payment_method, None)
+
+        payment_method = add_customer_payment_method(
+            FAKE_PAYMENT_METHOD_I["id"], self.user.customer_id
+        )
+        self.assertEqual(payment_method["customer"], self.user.customer_id)
+        customer = set_default_payment_method_for_customer(
+            FAKE_PAYMENT_METHOD_I["id"], self.user.customer_id
+        )
+        self.assertEqual(
+            customer["invoice_settings"]["default_payment_method"],
+            FAKE_PAYMENT_METHOD_I["id"],
+        )
+
+        self.assertIsNone(detach_payment_method(payment_method["id"]))
---
+++ src/apps/core/tests/test_messages.py
@@ -0,0 +1,94 @@
+import datetime
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from sqlalchemy import create_engine, delete, select
+from sqlalchemy.orm import sessionmaker
+
+from apps.core.outside_db.models import SmsItem
+from apps.core.outside_db.services import get_user_message_count
+
+
+user_model = get_user_model()
+USERDATA = {
+    "email": "softformanse@gmail.com",
+    "full_name": "softformanse test",
+    "password": "testpwd",
+}
+
+
+class TestMessageCountOnDashboard(TestCase):
+    def setUp(self) -> None:
+        engine = create_engine(settings.OUTSIDE_DATA_NETLOC)
+        self.Session = sessionmaker(engine)
+
+        self.user = user_model.objects.create(
+            full_name=USERDATA["full_name"],
+            email=USERDATA["email"],
+            twillion_number="123456",
+        )
+        self.user.is_active = True
+        self.user.set_password(USERDATA["password"])
+        self.user.save()
+
+        with self.Session() as session:
+            session.query(SmsItem).filter(
+                SmsItem.user_email == self.user.email
+            ).delete(synchronize_session=False)
+            session.commit()
+
+    def create_messages(self):
+        ids = [i for i in range(1, 13)]
+        with self.Session() as session:
+            items = [
+                SmsItem(
+                    sms_text=f"Test message {i}",
+                    user_email=self.user.email,
+                    sms_id=i * 10000,
+                    sms_date=datetime.datetime(2022, i, i),
+                    sender_phone_number=self.user.twillion_number,
+                    recipient_phone_number=self.user.twillion_number + f"{i}",
+                )
+                for i in ids
+            ]
+            session.add_all(items)
+
+            session.commit()
+
+    def test_message_count(self):
+        daily_message_count = get_user_message_count(
+            self.user.twillion_number, "day"
+        )
+        monthly_message_count = get_user_message_count(
+            self.user.twillion_number, "month"
+        )
+        yearly_message_count = get_user_message_count(
+            self.user.twillion_number, "year"
+        )
+
+        self.assertEqual(daily_message_count, 0)
+        self.assertEqual(monthly_message_count, 0)
+        self.assertEqual(yearly_message_count, 0)
+        self.create_messages()
+
+        daily_message_count = get_user_message_count(
+            self.user.twillion_number, "day"
+        )
+        monthly_message_count = get_user_message_count(
+            self.user.twillion_number, "month"
+        )
+        yearly_message_count = get_user_message_count(
+            self.user.twillion_number, "year"
+        )
+        self.assertEqual(daily_message_count, 1)
+        self.assertEqual(monthly_message_count, 1)
+        self.assertEqual(yearly_message_count, 12)
+
+    def tearDown(self):
+        with self.Session() as session:
+            session.query(SmsItem).filter(
+                SmsItem.user_email == self.user.email
+            ).delete(synchronize_session=False)
+            session.commit()
