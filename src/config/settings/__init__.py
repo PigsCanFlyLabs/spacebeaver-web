deb28db80af3c8d86f43298e8d4e9eefffec56a2
Holden Karau <holden@pigscanfly.ca>
2022-12-19 20:50:33 -0800

    mini progress

--- src/config/settings/__init__.py
+++ src/config/settings/__init__.py
@@ -5,5 +5,7 @@ from .test import Test
 from .unit_test import UnitTest
 from dotenv import load_dotenv, find_dotenv
 
-
-load_dotenv(find_dotenv())
+try:
+    load_dotenv(find_dotenv())
+except Exception as e:
+    print(f"Could not load {e}")
